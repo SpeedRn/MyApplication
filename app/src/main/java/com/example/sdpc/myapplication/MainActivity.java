c30073f65fbd4865e5934039131dc1ef8e09b649
Shao Dong <shaodong@le.com>
2016-08-05 18:06:43 +0800

    demo

--- app/src/main/java/com/example/sdpc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sdpc/myapplication/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.sdpc.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+import static android.support.v7.widget.LinearLayoutManager.HORIZONTAL;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getSimpleName();
+    RecyclerView rlvInUse;
+    RecyclerView rlvToAdd;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("SD has update this File");
-        System.out.println("SD has update this File AGAIG！！！But this time he will use commond line to commit this file  hohoho!!!");
-        System.out.println("Third time just for fun!");
-        System.out.println("Rename the remote repository!");
-        System.out.println("git branch test merge!");
+        rlvInUse = (RecyclerView) findViewById(R.id.rlv_in_use);
+        rlvInUse = (RecyclerView) findViewById(R.id.rlv_to_add);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        lm.setOrientation(LinearLayout.HORIZONTAL);
+        rlvInUse.setLayoutManager(lm);
+
+        rlvInUse.setAdapter();
     }
+
+    @Override
+    protected void  onPause() {
+        super.onPause();
+        Log.d(TAG,"onpause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
 }
---
+++ app/src/main/java/com/stv/launcher/dev/leakcanary/DefaultLeakDirectoryProvider.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2016 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stv.launcher.dev.leakcanary;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.Environment;
+
+import com.stv.launcher.dev.leakcanary.internal.RequestStoragePermissionActivity;
+
+import java.io.File;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.M;
+import static com.stv.launcher.dev.leakcanary.internal.LeakCanaryInternals.showNotification;
+
+public final class DefaultLeakDirectoryProvider implements LeakDirectoryProvider {
+
+  private final Context context;
+
+  public DefaultLeakDirectoryProvider(Context context) {
+    this.context = context.getApplicationContext();
+  }
+
+  @Override public File leakDirectory() {
+//    File downloadsDirectory = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS);
+    //TODO 使用应用data/data/.../file作为存储空间
+    File downloadsDirectory = context.getFilesDir();
+    File directory = new File(downloadsDirectory, "leakcanary-" + context.getPackageName());
+    boolean success = directory.mkdirs();
+    if (!success && !directory.exists()) {
+      throw new UnsupportedOperationException(
+          "Could not create leak directory " + directory.getPath());
+    }
+    return directory;
+  }
+
+  @Override public void requestWritePermissionNotification() {
+    if (hasStoragePermission()) {
+      return;
+    }
+    PendingIntent pendingIntent = RequestStoragePermissionActivity.createPendingIntent(context);
+    String contentTitle = context.getString(com.example.sdpc.myapplication.R.string.leak_canary_permission_notification_title);
+    CharSequence packageName = context.getPackageName();
+    String contentText =
+        context.getString(com.example.sdpc.myapplication.R.string.leak_canary_permission_notification_text, packageName);
+    showNotification(context, contentTitle, contentText, pendingIntent);
+  }
+
+  @TargetApi(M) @Override public void requestPermission(Activity activity) {
+    if (hasStoragePermission()) {
+      return;
+    }
+    String[] permissions = {
+        WRITE_EXTERNAL_STORAGE
+    };
+    activity.requestPermissions(permissions, 42);
+  }
+
+  @Override public boolean isLeakStorageWritable() {
+    if (!hasStoragePermission()) {
+      return false;
+    }
+    String state = Environment.getExternalStorageState();
+    return Environment.MEDIA_MOUNTED.equals(state);
+  }
+
+  @TargetApi(M) private boolean hasStoragePermission() {
+    if (SDK_INT < M) {
+      return true;
+    }
+    return context.checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PERMISSION_GRANTED;
+  }
+}
